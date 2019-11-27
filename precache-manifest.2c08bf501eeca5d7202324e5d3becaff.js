28f22b7bfd22285f48be2d284c3e4790a52f1a5c
Belga-JS <Meherzi.belga@yahoo.com>
2019-11-27 23:01:18 +0100

    Updates

---
+++ precache-manifest.2c08bf501eeca5d7202324e5d3becaff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a797ffb027df0243d79b880c01a6f0",
+    "url": "/dynamic-time/index.html"
+  },
+  {
+    "revision": "8ee354decb8fc90564a4",
+    "url": "/dynamic-time/static/css/main.ba8a9758.chunk.css"
+  },
+  {
+    "revision": "420ce79edeed60d776a7",
+    "url": "/dynamic-time/static/js/2.e7838432.chunk.js"
+  },
+  {
+    "revision": "8ee354decb8fc90564a4",
+    "url": "/dynamic-time/static/js/main.20fdb19e.chunk.js"
+  },
+  {
+    "revision": "a5486619e8c59b097ed6",
+    "url": "/dynamic-time/static/js/runtime-main.d9bbe867.js"
+  }
+]);
